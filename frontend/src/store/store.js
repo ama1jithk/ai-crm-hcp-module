bfa17adc9cd06a48dc1ea8cdff3fb04b54010125
ama1jithk <amaljithk3516@gmail.com>
2026-07-12 00:01:30 +0530

    Update store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import hcpReducer from "./hcpSlice";
 import interactionsReducer from "./interactionsSlice";
 import chatReducer from "./chatSlice";
+import repReducer from "./repSlice";
 
 export const store = configureStore({
   reducer: {
     hcps: hcpReducer,
     interactions: interactionsReducer,
     chat: chatReducer,
+    rep: repReducer,
   },
 });
