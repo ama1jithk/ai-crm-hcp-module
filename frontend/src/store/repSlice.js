12823a0b20469160a95e1f5a1923ca3e920c4818
ama1jithk <amaljithk3516@gmail.com>
2026-07-12 00:04:51 +0530

    Update repSlice.js

--- frontend/src/store/repSlice.js
+++ frontend/src/store/repSlice.js
@@ -1,1 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
 
+const repSlice = createSlice({
+  name: "rep",
+  initialState: {
+    name: "",
+  },
+  reducers: {
+    setRepName(state, action) {
+      state.name = action.payload;
+    },
+  },
+});
+
+export const { setRepName } = repSlice.actions;
+export default repSlice.reducer;
